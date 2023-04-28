3aa4f7f8b44a7cb122910b522bc2a52ab18c50d2
Beniamin Antal-Vaida <black_ben5@yahoo.com>
2023-04-28 09:40:33 +0300

    UberFoodClientApp

---
+++ UberFoodUser/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import HomeScreen from "./src/screens/HomeScreen";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <HomeScreen />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f3f3f3",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    paddingVertical: 30,
+  },
+});
---
+++ UberFoodUser/src/screens/HomeScreen/index.js
@@ -0,0 +1,15 @@
+import { StyleSheet, FlatList } from "react-native";
+import restaurants from "../../../assets/data/restaurants.json";
+import RestaurantItem from "../../components/RestaurantItem";
+
+export default function HomeScreen() {
+  return (
+    <FlatList
+      data={restaurants}
+      renderItem={({ item }) => <RestaurantItem restaurant={item} />}
+      showsVerticalScrollIndicator={false}
+    />
+  );
+}
+
+const styles = StyleSheet.create({});
